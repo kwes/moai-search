c3d7681334c1eaf0555545e8a813f08bcc4143b9
kwes <82199887+kwes@users.noreply.github.com>
2021-10-20 10:15:40 -0700

    Update moai-find.py

--- moai-find.py
+++ moai-find.py
@@ -6,11 +6,17 @@ import os
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 cls()
+
+blue = '\033[94m'
+cyan = '\033[96m'
+green = '\033[92m'
+end = '\033[0m'
+
 while True:
 
-  x = input("moai search - $ ")
+  x = input(f"{blue}moai search - {end}")
   if x == "search":
-    searchres = input("what do you wanna search for? - ")
+    searchres = input(" what do you wanna search for? - ")
     cls()
 
     browser = mechanicalsoup.StatefulBrowser()
@@ -28,10 +34,14 @@ while True:
             target = re.sub(r"^/url\?q=([^&]*)&.*", r"\1", target)
             print(target)
   elif x == "open":
-    res = input("type the url - https://")
+    res = input(f"{green} type the url - https://{end}")
     cls()
     url = "https://" + res
     html = urllib.request.urlopen(url).read().decode('utf-8')
 
     text = get_text(html)
     print(text)
+  elif x == "help":
+    print(" open - type in the url to go to the page \n search - type in keywords to find most relevant url's")
+  else:
+      print(" invalid command, type 'help'")
